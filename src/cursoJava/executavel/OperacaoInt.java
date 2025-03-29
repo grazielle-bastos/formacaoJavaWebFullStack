e643e73628a45138d0d7f06b512938671dfc743d
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-29 20:18:35 -0300

    Adição de operação com números tipo double

--- src/cursoJava/executavel/OperacaoInt.java
+++ src/cursoJava/executavel/OperacaoInt.java
@@ -4,14 +4,25 @@ public class OperacaoInt {
 	
 	public static void main(String[] args) {
 		
-		/* - Operação entre números inteiros */
+		/* - Operação entre números naturais - Tipo inteiros */
 		
 		int nota1 = 90;
 		int nota2 = 70;
 		int nota3 = 50;
 		int nota4 = 88;
 		
-		System.out.println("Média das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);		
+		System.out.println("Média das notas em número inteiro = " + (nota1 + nota2 + nota3 + nota4) / 4);		
+		
+		/* - Operação entre números reais - Tipo double (carrega números decimais) */
+		
+		double nota5 = 90;
+		double nota6 = 70;
+		double nota7 = 50;
+		double nota8 = 88;
+		
+		System.out.println("Média das notas em número decimal = " + (nota5 + nota6 + nota7 + nota8) / 4);		
+		
+		
 	}
 
 }
