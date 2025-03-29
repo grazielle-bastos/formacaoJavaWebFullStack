2a7fb6015a9d933a7815666dbf2727eb6ba9d0a6
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-29 19:55:35 -0300

    Adição da variável local e variável global

--- src/cursoJava/executavel/PrimeiraClasseJava.java
+++ src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -2,6 +2,9 @@ package cursoJava.executavel;
 
 public class PrimeiraClasseJava {
 
+	// variável global
+	static int maiorIdadeGlobal = 30;
+
 	public static void main(String[] args) {
 
 		/*
@@ -12,10 +15,19 @@ public class PrimeiraClasseJava {
 
 		/* Imprime a mensagem no console | Escreve o texto no console */
 		System.out.println("Meu primeiro programa em Java do curso 'Formação Java Web', JDEV Treinamento");
-		System.out.println("\n\n" + linguagem + " é orientado a objetos");
-		
-		//Criado nova branch		
+		System.out.println("\n\n" + linguagem + " é orientado a objetos \n\n---\n");
 
+		// Criado nova branch
+
+		// variável local
+		int maiorIdade = 18;
+		System.out.println("Valor da variável local = " + maiorIdade);
+
+		metodo2();
+	}
+
+	public static void metodo2() {
+		System.out.println("Valor da variável Global = " + maiorIdadeGlobal);
 	}
 
 }
