35781eeff8403d3cd865e9ddc981af905f9f0388
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-29 16:49:23 -0300

    Criação do projeto

---
+++ src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,19 @@
+package cursoJava.executavel;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Main é um método auto executável em Java
+		 */
+
+		String linguagem = "Java";
+
+		/* Imprime a mensagem no console | Escreve o texto no console */
+		System.out.println("Meu primeiro programa em Java do curso 'Formação Java Web', JDEV Treinamento");
+		System.out.println("\n\n" + linguagem + " é orientado a objetos");
+
+	}
+
+}
---
+++ src/treinoGit/jdev/Pessoa.java
@@ -0,0 +1,50 @@
+package treinoGit.jdev;
+
+import java.util.Date;
+
+public class Pessoa {
+	
+	/*Atributo*/
+	private String nome;
+	
+	private String sobrenome;
+	
+	private int idade;
+	
+	private Date dataNascimento;
+	
+	private Date dataCadastro;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
