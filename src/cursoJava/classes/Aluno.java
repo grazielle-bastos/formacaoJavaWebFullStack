246b22f549701eb0f3c44b15eecebb22de3fb0a8
Grazielle Bastos <grazielle1101@gmail.com>
2025-04-11 22:59:10 -0300

    Adição de novos atributos (disciplina) do objeto Aluno.

--- src/cursoJava/classes/Aluno.java
+++ src/cursoJava/classes/Aluno.java
@@ -26,9 +26,13 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	private double nota1;
+	private String disciplina1;
 	private double nota2;
+	private String disciplina2;
 	private double nota3;
+	private String disciplina3;
 	private double nota4;
+	private String disciplina4;
 
 	/*
 	 * Controle de acesso a variáveis.
@@ -165,6 +169,39 @@ public class Aluno {
 	public void setNota4(double nota4) {
 		this.nota4 = nota4;
 	}
+	
+
+	public String getDisciplina1() {
+		return disciplina1;
+	}
+
+	public void setDisciplina1(String disciplina1) {
+		this.disciplina1 = disciplina1;
+	}
+
+	public String getDisciplina2() {
+		return disciplina2;
+	}
+
+	public void setDisciplina2(String disciplina2) {
+		this.disciplina2 = disciplina2;
+	}
+
+	public String getDisciplina3() {
+		return disciplina3;
+	}
+
+	public void setDisciplina3(String disciplina3) {
+		this.disciplina3 = disciplina3;
+	}
+
+	public String getDisciplina4() {
+		return disciplina4;
+	}
+
+	public void setDisciplina4(String disciplina4) {
+		this.disciplina4 = disciplina4;
+	}
 
 	/* Método que retorna a média do aluno */
 	public double getMediaNota() {
@@ -190,15 +227,18 @@ public class Aluno {
 		}
 	}
 
-	@Override
-	public String toString() {
-		return "Aluno [nome=" + getNome() + ", idade=" + getIdade() + ", dataNascimento=" + dataNascimento
-				+ ", registroGeral=" + registroGeral + ", numeroCPF=" + numeroCPF + ", nomeMae=" + nomeMae
-				+ ", nomePai=" + nomePai + ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola
-				+ ", serieMatriculado=" + serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3="
-				+ nota3 + ", nota4=" + nota4 + "]";
-	}
+
 	
+	  @Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCPF=" + numeroCPF + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", nota1=" + nota1 + ", disciplina1=" + disciplina1 + ", nota2=" + nota2
+				+ ", disciplina2=" + disciplina2 + ", nota3=" + nota3 + ", disciplina3=" + disciplina3 + ", nota4="
+				+ nota4 + ", disciplina4=" + disciplina4 + "]";
+	}
+
 	  @Override public int hashCode() { return Objects.hash(nome, numeroCPF); }
 	  
 	  @Override public boolean equals(Object obj) { if (this == obj) return true;
---
+++ src/cursoJava/executavel/ClasseJavaIII_Disciplinas.java
@@ -0,0 +1,77 @@
+package cursoJava.executavel;
+
+import javax.swing.JOptionPane;
+
+import cursoJava.classes.Aluno;
+
+public class ClasseJavaIII_Disciplinas {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Entrada de dados
+		 */
+		String nome = JOptionPane.showInputDialog("Digite o nome do primeiro aluno: ");
+		String idade = JOptionPane.showInputDialog("Digite a idade do primeiro aluno: ");
+		String dataNascimento = JOptionPane.showInputDialog("Digite a data de nascimento do primeiro aluno: ");
+		String RG = JOptionPane.showInputDialog("Digite o RG do primeiro aluno: ");
+		String CPF = JOptionPane.showInputDialog("Digite o CPF do primeiro aluno: ");
+		String nomeMae = JOptionPane.showInputDialog("Digite o nome da mãe do primeiro aluno: ");
+		String nomePai = JOptionPane.showInputDialog("Digite o nome do pai do primeiro aluno: ");
+		String dataMatricula = JOptionPane.showInputDialog("Digite a data da matrícula do primeiro aluno: ");
+		String serieMatriculado = JOptionPane.showInputDialog("Digite a série matriculado do primeiro aluno: ");
+		String escola = JOptionPane.showInputDialog("Digite a escola do primeiro aluno: ");
+		
+		String disciplina1 = JOptionPane.showInputDialog("Informe a disciplina 1: ");
+		String nota1 = JOptionPane.showInputDialog("Digite a nota 1 do primeiro aluno: ");
+		
+		String disciplina2 = JOptionPane.showInputDialog("Informe a disciplina 1: ");
+		String nota2 = JOptionPane.showInputDialog("Digite a nota 2 do primeiro aluno: ");
+		
+		String disciplina3 = JOptionPane.showInputDialog("Informe a disciplina 1: ");
+		String nota3 = JOptionPane.showInputDialog("Digite a nota 3 do primeiro aluno: ");
+		
+		String disciplina4 = JOptionPane.showInputDialog("Informe a disciplina 4: ");
+		String nota4 = JOptionPane.showInputDialog("Digite a nota 4 do primeiro aluno: ");
+
+		/*
+		 * Instanciar objeto pra depois passar os dados
+		 */
+		Aluno aluno1 = new Aluno();
+
+		/*
+		 * Executar e passar os dados da tela
+		 */
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade)); // Pacote que converte a string para tipo inteiro
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(RG);
+		aluno1.setNumeroCPF(CPF);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setNomePai(nomePai);
+		aluno1.setDataMatricula(dataMatricula);
+		aluno1.setSerieMatriculado(serieMatriculado);
+		aluno1.setNomeEscola(escola);
+		
+		aluno1.setNota1(Double.parseDouble(nota1)); // Pacote que converte a string para tipo double
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
+		
+		aluno1.setDisciplina1(disciplina1);
+		aluno1.setDisciplina2(disciplina2);
+		aluno1.setDisciplina3(disciplina3);
+		aluno1.setDisciplina4(disciplina4);
+		
+
+		/*
+		 * Imprimir os dados no console
+		 */
+		System.out.println(aluno1.toString()); // Descrição dp objeto na memória
+		System.out.println("Média do aluno = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
+
+		
+	}
+
+}
